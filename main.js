d5c8c9dad4aafa105e9815dc03e1e6543cc479dc
Sayyoddev <ibrohimovsayyodbek6@gmail.com>
2023-07-29 11:23:58 +0500

    snakegame commit

---
+++ main.js
@@ -0,0 +1,124 @@
+// canvas
+const canvas = document.getElementById('canvas')
+// canvas 2d
+const context = canvas.getContext('2d');
+
+// uyin maydonidagi kataklar ulchami
+var grid = 16;
+var count = 0;
+var score = 0;
+var max = 0;
+
+var snake = {
+    x: 160,
+    y: 160,
+
+    dx: grid,
+    dy: 0,
+
+    maxCells: 1,
+
+    cells: [],
+}
+
+var food = {
+    x: 320,
+    y: 320,
+}
+
+function getRandomInt(min, max){
+    return Math.floor(Math.random() * (max-min)) + min;
+}
+
+function loop() {
+    requestAnimationFrame(loop)
+    if(++count < 4){ //60fps /15fps = 4
+        return;
+    }
+
+    count= 0;
+    context.clearRect(0,0, canvas.clientWidth, canvas.clientHeight);
+
+    snake.x += snake.dx;
+    snake.y += snake.dy;
+
+    if(snake.x < 0){ //goriz
+        snake.x = canvas.clientWidth - grid;
+    }
+    else if(snake.x >= canvas.clientWidth){
+        snake.x = 0;
+    }
+
+    if(snake.y < 0){ //vertikal
+        snake.y= canvas.clientHeight - grid;
+    }
+    else if(snake.y >= canvas.clientHeight){
+        snake.y = 0;
+    }
+
+    snake.cells.unshift({ x: snake.x, y:snake.y });
+
+    if(snake.cells.length > snake.maxCells){
+        snake.cells.pop();
+    }
+
+    // ovqat rangi
+    context.fillStyle = '#fff';
+    context.fillRect(food.x, food.y, grid - 1, grid-1);
+
+    // snake rangi
+    context.fillStyle = '#E43F5A';
+
+    snake.cells.forEach(function (cell, index){
+        context.fillRect(cell.x, cell.y, grid - 1, grid - 1)
+        if(cell.x === food.x && cell.y === food.y){
+            // ilonni uzunligi
+            snake.maxCells++;
+            // ochko
+            score +=1;
+            document.getElementById('score').innerHTML = score;
+
+            food.x = getRandomInt(0, 25) * grid;
+            food.y = getRandomInt(0, 25) * grid;
+        }
+        for(var i =index+1; i < snake.cells.length; i++){
+            if(cell.x === snake.cells[i].x && cell.y === snake.cells[i].y){
+                if(score > max){
+                    max=score;
+                }
+                snake.x = 160;
+                snake.y = 160;
+                snake.cells = [];
+                snake.maxCells = 1;
+                snake.dx = grid;
+                snake.dy = 0;
+                score = 0;
+                food.x = getRandomInt(0, 25) * grid;
+                food.y = getRandomInt(0, 25) * grid;
+
+                document.getElementById('score').innerHTML = max;
+            }
+        }
+    })
+}
+
+document.addEventListener('keydown', function(e){
+    if(e.keyCode === 37 && snake.dx === 0){ //chap
+        snake.dx = -grid;
+        snake.dy = 0;
+    }
+    else if(e.keyCode === 38 && snake.dy === 0){ //tepa
+        snake.dy = -grid;
+        snake.dx = 0;
+    }
+    else if(e.keyCode === 39 && snake.dx === 0){ //o'ng
+        snake.dx = grid;
+        snake.dy = 0;
+    }
+    else if(e.keyCode === 40 && snake.dy === 0){ //past
+        snake.dy = grid;
+        snake.dx = 0;
+    }
+})
+
+requestAnimationFrame(loop)
